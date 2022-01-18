382fe91ba6d5cb558d1b8fef26e82b3807b6eefe
RhythmiCon <conormcguigan.code@gmail.com>
2022-01-18 14:09:53 +0000

    testing frontend and backend connection

--- wellnessapp/src/Components/App/index.js
+++ wellnessapp/src/Components/App/index.js
@@ -1,28 +1,36 @@
-
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       {/* <img src= className="App-logo" alt="logo" />  */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+   const [timerData, setTimerData] = useState([]);
 
+   async function getTimers() {
+      const response = await fetch("/timers");
+      let data = await response.json();
+      setTimerData([...timerData, data]);
+      console.log(data);
+   }
 
-        
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   useEffect(() => getTimers());
+
+   return (
+      <div className="App">
+         <header className="App-header">
+            {/* <img src= className="App-logo" alt="logo" />  */}
+            <p>
+               Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+               className="App-link"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               Learn React
+            </a>
+         </header>
+      </div>
+   );
 }
 
 export default App;
