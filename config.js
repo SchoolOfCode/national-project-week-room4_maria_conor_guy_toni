63825f40ffb7d4af88d7b62b43641a934fb93d81
Tonisaraiva <scramtoni@gmail.com>
2022-01-18 10:24:02 +0000

    with db files setup and postgres added

---
+++ config.js
@@ -0,0 +1,9 @@
+export const db = {
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT
+    };
+    
+    
