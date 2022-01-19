d6b91d6a529f0dc61eb385a864f39bab10daa2ac
Maria-C1 <mariacrawford789@gmail.com>
2022-01-19 11:47:26 +0000

    stopwatch working!

--- wellnessapp/src/Components/App/index.js
+++ wellnessapp/src/Components/App/index.js
@@ -3,28 +3,30 @@ import Logo from "../Logo";
 import PageHeading from "../PageHeading";
 import PageSubheading from "../PageSubheading";
 import "./App.css";
+import Stopwatch from "../Stopwatchapp";
 
 function App() {
-   const [timerData, setTimerData] = useState([]);
+  const [timerData, setTimerData] = useState([]);
 
-   async function getTimers() {
-      const response = await fetch("/timers");
-      let data = await response.json();
-      setTimerData([...timerData, data]);
-      console.log(data);
-   }
+  async function getTimers() {
+    const response = await fetch("/timers");
+    let data = await response.json();
+    setTimerData([...timerData, data]);
+    console.log(data);
+  }
 
-   useEffect(() => {
-      getTimers();
-   }, []);
+  useEffect(() => {
+    getTimers();
+  }, []);
 
-   return (
-      <div className="App">
-         <Logo/>
-         <PageHeading/>
-         <PageSubheading/>
-      </div>
-   );
+  return (
+    <div className="App">
+      <Logo />
+      <PageHeading />
+      <PageSubheading />
+      <Stopwatch />
+    </div>
+  );
 }
 
 export default App;
