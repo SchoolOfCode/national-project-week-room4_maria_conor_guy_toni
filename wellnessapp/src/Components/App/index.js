eec01bf8bd825a14010f81060d52482f28021d97
Guy-Robbins <guy.robbins1@gmail.com>
2022-01-19 14:04:54 +0000

    worked on api

--- wellnessapp/src/Components/App/index.js
+++ wellnessapp/src/Components/App/index.js
@@ -2,16 +2,23 @@ import { useEffect, useState } from "react";
 import Logo from "../Logo";
 import PageHeading from "../PageHeading";
 import PageSubheading from "../PageSubheading";
+import QuoteText from "../QuoteText";
 import "./App.css";
 
 function App() {
    const [timerData, setTimerData] = useState([]);
-
    async function getTimers() {
       const response = await fetch("/timers");
       let data = await response.json();
-      setTimerData([...timerData, data]);
-      console.log(data);
+      console.log(data.payload);
+      setTimerData(data.payload);
+      //console.log(data);
+      console.log(setTimerData);
+   // async function getTimers() {
+   //    const response = await fetch("/timers");
+   //    let data = await response.json();
+   //    setTimerData([...timerData, data]);
+   //    console.log(data);
    }
 
    useEffect(() => {
@@ -23,6 +30,7 @@ function App() {
          <Logo/>
          <PageHeading/>
          <PageSubheading/>
+         <QuoteText/>
       </div>
    );
 }
--- wellnessapp/src/Components/QuoteText/index.js
+++ wellnessapp/src/Components/QuoteText/index.js
@@ -3,16 +3,30 @@ import React, {useEffect, useState} from 'react'
 const QuoteText = () => {
 // need to fetch text from a quotes api and return it in a box
 
-const [QuoteData, setQuoteData] = useState("");
+const [quoteData, setQuoteData] = useState({});
 
+useEffect(() => {
 async function getQuote() {
-   const response = await fetch("https://zenquotes.io/api/random");
-   let data = await response.json();
-   setQuoteData([...QuoteData, data]);
+    // event.preventDefault()
+
+
+    let response = await fetch("https://bodybuilding-quotes1.p.rapidapi.com/quotes?page=1", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "bodybuilding-quotes1.p.rapidapi.com",
+            "x-rapidapi-key": "727e08c480msh9b9088d53e375bep189ddfjsn01220a09870a"
+        }
+    });
+    let data = await response.json();
+// var quotes = data[Math.floor(Math.random()*data.length)];
+//    const response = await fetch("https://zenquotes.io/api/random",{ mode: 'no-cors'});
+//    let data = await response.json();
+   setQuoteData(data[0]);
+   
    console.log(data);
 }
 
-useEffect(() => {
+
    getQuote();
 }, []);
 
@@ -20,7 +34,7 @@ useEffect(() => {
 
     return (
         <div>
-           
+           {quoteData.quote}
         </div>
     )
 }
