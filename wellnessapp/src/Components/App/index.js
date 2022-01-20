756660b9bc065585ebd54142f14c7a029e0088de
Tonisaraiva <scramtoni@gmail.com>
2022-01-20 11:10:08 +0000

    with pwn api displaying text

--- wellnessapp/src/Components/App/index.js
+++ wellnessapp/src/Components/App/index.js
@@ -21,6 +21,25 @@ function App() {
 
    console.log(timerData);
 
+
+   // const [quotesData, setQuotesData] = useState([]);
+   // async function getQuotes() {
+   //    const response = await fetch("/quotes");
+   //    const data = await response.json();
+   //    setQuotesData([...quotesData, data.payload]);
+   // }
+
+   // useEffect(() => {
+   //    getQuotes();
+   // }, [quotesData]);
+
+   // console.log(quotesData);
+
+
+
+
+
+
    return (
       <div className="App">
          <Logo />
@@ -40,7 +59,22 @@ function App() {
                ))
             )}
          </div>
-         <QuoteText />
+         <div className="quotes">
+
+         
+         {/* {quotesData.length === 0 ? (
+               <p>Loading</p>
+            ) : (
+               quotesData[0].map(({ quote, author }, index) => (
+                  <QuoteText
+                     key={index}
+                     quote={quote}
+                     author={author}
+                  />
+               ))
+            )} */}
+<QuoteText />
+         </div>
          <Footer />
       </div>
    );
--- wellnessapp/src/Components/QuoteText/index.js
+++ wellnessapp/src/Components/QuoteText/index.js
@@ -1,42 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import "./index.css";
 
+
 const QuoteText = () => {
-// need to fetch text from a quotes api and return it in a box
+const [quotesData, setQuotesData] = useState([]);
+   async function getQuotes() {
+      const response = await fetch("/quotes");
+      const data = await response.json();
+      setQuotesData([...quotesData, data.payload]);
+   }
 
-const [quoteData, setQuoteData] = useState({});
+   useEffect(() => {
+      getQuotes();
+   }, []);
 
-useEffect(() => {
-async function getQuote() {
+   console.log(quotesData);
 
-
-    let response = await fetch("https://bodybuilding-quotes1.p.rapidapi.com/quotes?page=2", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "bodybuilding-quotes1.p.rapidapi.com",
-            "x-rapidapi-key": "727e08c480msh9b9088d53e375bep189ddfjsn01220a09870a"
-        }
-    });
-    let data = await response.json();
-
-
-
-//    const response = await fetch("https://zenquotes.io/api/random",{ mode: 'no-cors'});
-//    let data = await response.json();
-   setQuoteData(data[Math.floor(Math.random()*data.length)+1]);
-   
-   console.log(data);
-}
-
-
-   getQuote();
-}, []);
-
-
-
+const random = Math.floor(Math.random()*quotesData.length)+1
     return (
         <div>
-           {quoteData.quote}
+        {quotesData.length === 0 ? (
+               <p>Loading</p>) : (
+                   quotesData[0][random].quote)}
+{/* quotesData[0][random].quote  quotesData[0][random].author
+               )} */}
         </div>
     )
 }
