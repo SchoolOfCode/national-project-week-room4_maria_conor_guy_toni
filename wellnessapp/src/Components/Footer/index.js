bfee48450e67402ea5233e91de1c08645c6d019f
Tonisaraiva <scramtoni@gmail.com>
2022-01-20 12:26:05 +0000

    changed the footer infooter

--- wellnessapp/src/Components/Footer/index.js
+++ wellnessapp/src/Components/Footer/index.js
@@ -12,28 +12,28 @@ const Footer = () => {
          <div className="footer__text">
             <p>
                <a>Ts&Cs</a>|
-               <a href="mailto:anne-marie@schoolofcode.com">Contact</a>
+               <a href="mailto:anne-marie@schoolofcode.com?subject=I might need some help&body=Hello Anne-Marie">Contact</a>
             </p>
          </div>
          <div className="footer__images">
+         <a href="https://www.samaritans.org/" target="_blank" rel="noreferer">
             <img
                className="footer__logo"
                src={samaritans}
                alt="Samaritans logo"
                width="100px"
                height="100px"
-               href="https://www.samaritans.org/"
-               target="_blank"
-            />
+              
+               
+            /></a>
+            <a href="https://www.mind.org.uk" target="_blank" rel="noreferer">
             <img
                className="footer__logo mind__logo"
                src={mind}
                alt="Mind Logo"
                width="100px"
                height="100px"
-               href="https://www.mind.org.uk/"
-               target="_blank"
-            />
+            /></a>
          </div>
       </div>
    );
