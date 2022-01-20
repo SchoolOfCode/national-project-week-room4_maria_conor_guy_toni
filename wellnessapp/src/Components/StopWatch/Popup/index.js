9eb9591e951c45571be85a07a41c4ab53c8758e1
Maria-C1 <mariacrawford789@gmail.com>
2022-01-20 17:02:49 +0000

    alert plus new text. Needs title variable adding

--- wellnessapp/src/Components/StopWatch/Popup/index.js
+++ wellnessapp/src/Components/StopWatch/Popup/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
+import timer from "../../../../../db/data/timer-data";
 import "./style.css";
 
-function Popup({ children, setTrigger, popupState }) {
+function Popup({ children, setTrigger, popupState, MetricText }) {
   const [timedPopup, setTimedPopup] = useState(false);
   setTrigger = setTimedPopup;
   useEffect(() => {
@@ -17,7 +18,9 @@ function Popup({ children, setTrigger, popupState }) {
           close
         </button>
         {children}
-        <p>Remember to look after yourself! Your timer is almost finished.</p>
+        <p>
+          Remember to look after yourself! It's almost time that you {timer[0].title}
+        </p>
       </div>
     </div>
   ) : (
