0864876a00b80113ea4dd8eda436035d40613d52
Maria-C1 <mariacrawford789@gmail.com>
2022-01-20 16:42:01 +0000

    alerts working, css needed

--- wellnessapp/src/Components/App/index.js
+++ wellnessapp/src/Components/App/index.js
@@ -8,51 +8,47 @@ import Stopwatch from "../StopWatch";
 import Footer from "../Footer";
 
 function App() {
-   const [timerData, setTimerData] = useState([]);
-   async function getTimers() {
-      const response = await fetch("/timers");
-      const data = await response.json();
-      setTimerData([...timerData, data.payload]);
-   }
+  const [timerData, setTimerData] = useState([]);
+  async function getTimers() {
+    const response = await fetch("/timers");
+    const data = await response.json();
+    setTimerData([...timerData, data.payload]);
+  }
 
-   useEffect(() => {
-      getTimers();
-   }, []);
+  useEffect(() => {
+    getTimers();
+  }, []);
 
-   console.log(timerData);
+  console.log(timerData);
 
-   return (
-      <div className="App">
-         <Logo />
-         <div className="headers">
-            <PageHeading />
-            <PageSubheading />
-         </div>
-         <div className="timers">
-            {timerData.length === 0 ? (
-               <p>Loading</p>
-            ) : (
-               timerData[0].map(({ title, warning, overdue }, index) => (
-                  <Stopwatch
-                     key={index}
-                     metric={title}
-                     warning={warning}
-                     overdue={overdue}
-                  />
-               ))
-            )}
-         </div>
-
-         <div className="quote__container">
-            <QuoteText />
-         </div>
-         <div>
-            {/*        <button onClick={() => setButtonPopup(true)}>Open Popup</button> */}
-         </div>
-
-         <Footer />
+  return (
+    <div className="App">
+      <Logo />
+      <div className="headers">
+        <PageHeading />
+        <PageSubheading />
       </div>
-   );
+      <div className="timers">
+        {timerData.length === 0 ? (
+          <p>Loading</p>
+        ) : (
+          timerData[0].map(({ title, warning, overdue }, index) => (
+            <Stopwatch
+              key={index}
+              metric={title}
+              warning={warning}
+              overdue={overdue}
+            />
+          ))
+        )}
+      </div>
+
+      <div className="quote__container">
+        <QuoteText />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- wellnessapp/src/Components/StopWatch/Popup/index.js
+++ wellnessapp/src/Components/StopWatch/Popup/index.js
@@ -2,26 +2,27 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 
 function Popup({ children, setTrigger, popupState }) {
-   const [timedPopup, setTimedPopup] = useState(false);
+  const [timedPopup, setTimedPopup] = useState(false);
+  setTrigger = setTimedPopup;
+  useEffect(() => {
+    setTimeout(() => {
+      setTimedPopup(popupState);
+    });
+  }, [popupState]);
 
-   useEffect(() => {
-      setTimeout(() => {
-         setTimedPopup(popupState);
-      });
-   }, [popupState]);
-
-   return timedPopup ? (
-      <div className="popup">
-         <div className="popup-inner">
-            <button className="close-btn" onClick={() => setTrigger(false)}>
-               close
-            </button>
-            {children}
-         </div>
+  return timedPopup ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <button className="close-btn" onClick={() => setTrigger(false)}>
+          close
+        </button>
+        {children}
+        <p>Remember to look after yourself! Your timer is almost finished.</p>
       </div>
-   ) : (
-      ""
-   );
+    </div>
+  ) : (
+    ""
+  );
 }
 
 export default Popup;
