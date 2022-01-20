2a749f579b553dd598d1afc1dbbf2017508c22d3
Tonisaraiva <scramtoni@gmail.com>
2022-01-20 11:57:29 +0000

    with index of quotetext changed

--- wellnessapp/src/Components/QuoteText/index.js
+++ wellnessapp/src/Components/QuoteText/index.js
@@ -2,44 +2,6 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
 
-const QuoteText = () => {
-   // need to fetch text from a quotes api and return it in a box
-
-   const [quoteData, setQuoteData] = useState({});
-
-   useEffect(() => {
-      async function getQuote() {
-         let response = await fetch(
-            "https://bodybuilding-quotes1.p.rapidapi.com/quotes?page=1",
-            {
-               method: "GET",
-               headers: {
-                  "x-rapidapi-host": "bodybuilding-quotes1.p.rapidapi.com",
-                  "x-rapidapi-key":
-                     "727e08c480msh9b9088d53e375bep189ddfjsn01220a09870a",
-               },
-            }
-         );
-         let data = await response.json();
-
-         // var quotes = data[Math.floor(Math.random()*data.length)];
-
-         //    const response = await fetch("https://zenquotes.io/api/random",{ mode: 'no-cors'});
-         //    let data = await response.json();
-         setQuoteData(data[Math.floor(Math.random() * data.length) + 1]);
-
-
-         console.log(data);
-      }
-
-
-      getQuote();
-   }, []);
-
-   return <div className="quote__text">{quoteData.quote}</div>;
-};
-
-export default QuoteText;
 
 const QuoteText = () => {
 const [quotesData, setQuotesData] = useState([]);
@@ -57,7 +19,8 @@ const [quotesData, setQuotesData] = useState([]);
 
 const random = Math.floor(Math.random()*25)
     return (
-        <div>
+        <div className="quote__text">
+
         {quotesData.length === 0 ? (
                <p>Loading</p>) : (
                 
