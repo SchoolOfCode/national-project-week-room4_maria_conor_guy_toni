ee4c7f51730d7e831d1dcc88d957388b7d65b3f2
Tonisaraiva <scramtoni@gmail.com>
2022-01-21 06:17:53 +0000

    tooltip box first test

--- wellnessapp/src/Components/Logo/index.js
+++ wellnessapp/src/Components/Logo/index.js
@@ -2,8 +2,13 @@ import img from "../../images/School-of-Wellness.png";
 import "./index.css";
 
 function Logo(){
-    return (<div>
+    return (
+    <><div className="tooltip-on-hover">
+    <a href="https://schoolofcode.co.uk/" target="_blank" rel="noreferrer">
         <img className = "logo" src = {img} alt="School of code wellness logo"/>
-    </div>)
+        </a>
+    </div>
+    <div className="tooltip">Click to go to the School of Code website</div>
+    </>)
 }
 export default Logo
